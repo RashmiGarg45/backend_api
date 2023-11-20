23851ee5f39c4c050342f619911060fcc11f2671
rash_bitbucket <rashmi@appanalytics.in>
2023-11-20 14:39:12 +0530

    debug status changed

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("get_signature/", include("java_signatures.urls")),
+    path("backend/get_signature/", include("java_signatures.urls")),
     path('admin/', admin.site.urls),
 ]
