77e2a3dd1cb5f54bbc8dcc4023896864384d2153
rash_bitbucket <rashmi@appanalytics.in>
2023-12-19 15:29:57 +0530

    event count api's added

--- java_signatures/urls.py
+++ java_signatures/urls.py
@@ -4,5 +4,8 @@ from java_signatures import views
 urlpatterns = [
     path("get_signature/", views.get_signtaure, name="get_signtaure"),
     path("get_tatapalette_orders/", views.get_tatapalette_orders, name="get_tatapalette_orders"),
-    path("get_available_orders_count/", views.get_available_orders_count, name="get_available_orders_count")
+    path("get_available_orders_count/", views.get_available_orders_count, name="get_available_orders_count"),
+    path("add_install_count/", views.add_install_count, name="add_install_count"),
+    path("update_event_count/", views.update_event_count, name="update_event_count"),
+    path("event_allowed/", views.is_event_allowed, name="is_event_allowed")
 ]
--- java_signatures/views.py
+++ java_signatures/views.py
@@ -3,6 +3,10 @@ import subprocess
 import json
 from subprocess import STDOUT, PIPE
 import sqlite3
+import datetime
+import time
+import mysql.connector as mysql
+from rest_framework.decorators import api_view
 
 def get_signtaure(request):
     data = json.loads(request.body)
@@ -48,3 +52,95 @@ def get_available_orders_count(request):
         count = row[0]
         break
     return HttpResponse(count)
+
+@api_view(['POST'])
+def add_install_count(request):
+    request_data = json.loads(request.body)
+    campaign_name = request_data.get("name")
+    channel = request_data.get("channel")
+    network = request_data.get("network")
+    offer_id = request_data.get("offer_id")
+
+    try:
+        conn = mysql.connect(host="rds-datapis.cd89nha3un9e.us-west-2.rds.amazonaws.com", user="team2backend", passwd="123admin!", database="techteam")
+        cursor = conn.cursor()
+
+        created_at = datetime.datetime.fromtimestamp(time.time()).strftime("%d-%m-%Y %H:%M:%S")
+        cursor.execute('''INSERT INTO check_event_count (created_at, campaign_name, channel, network, offer_id)
+                        VALUES ('{}', '{}', '{}', '{}', '{}')'''.format(created_at, campaign_name, channel, network, offer_id))
+        conn.commit()
+
+        response_code = 200
+        message = "success"
+        data = {"created_at": str(created_at)}
+
+    except Exception as e:
+        response_code = 500
+        message = str(e)
+        data = {}
+
+    return HttpResponse(json.dumps({"response_code": response_code, "message": message, "data": data}))
+
+def is_event_allowed(request):
+    request_data = json.loads(request.body)
+    campaign_name = request_data.get("name")
+    event_type = request_data.get("event_type")
+    current_date = request_data.get("current_date")
+    channel = request_data.get("channel")
+    network = request_data.get("network")
+    offer_id = request_data.get("offer_id")
+    required_percentage = request_data.get("required_percentage")
+
+    try:
+        conn = mysql.connect(host="rds-datapis.cd89nha3un9e.us-west-2.rds.amazonaws.com", user="team2backend", passwd="123admin!", database="techteam")
+        cursor = conn.cursor()
+
+        cursor.execute('''SELECT COUNT(*) FROM check_event_count WHERE campaign_name ='{}' AND channel='{}' AND network='{}' AND offer_id='{}' AND created_at LIKE '{}%';'''.format(campaign_name, channel, network, offer_id, current_date))
+        result = cursor.fetchall()
+        install_count = result[0][0]
+
+        cursor.execute('''SELECT COUNT(*) FROM check_event_count WHERE campaign_name ='{}' AND channel='{}' AND network='{}' AND offer_id='{}' AND created_at LIKE '{}%' AND {} = 1;'''.format(campaign_name, channel, network, offer_id,current_date, event_type))
+        result = cursor.fetchall()
+        event_count = result[0][0]
+
+        current_percentage = (event_count/install_count)*100
+        if current_percentage < required_percentage.get(event_type):
+            is_allowed = True
+        else:
+            is_allowed = False 
+        
+        response_code = 200
+        message = "success"
+        data = {"is_allowed": is_allowed, "install_count": install_count, "event_count": event_count, "current_percentage": current_percentage}
+
+    except Exception as e:
+        response_code = 500
+        message = str(e)
+        data = {}   
+
+    return HttpResponse(json.dumps({"response_code": response_code, "message": message, "data": data}))
+
+@api_view(['POST'])
+def update_event_count(request):
+    request_data = json.loads(request.body)
+    campaign_name = request_data.get("name")
+    created_at = request_data.get("created_at")
+    event_origin = request_data.get("event_origin")
+    channel = request_data.get("channel")
+    network = request_data.get("network")
+    offer_id = request_data.get("offer_id")
+
+    try:
+        conn = mysql.connect(host="rds-datapis.cd89nha3un9e.us-west-2.rds.amazonaws.com", user="team2backend", passwd="123admin!", database="techteam")
+        cursor = conn.cursor()
+        cursor.execute('''UPDATE check_event_count SET {} = 1 WHERE created_at = '{}' AND campaign_name ='{}' AND channel='{}' AND network='{}' AND offer_id='{}';'''.format(event_origin, created_at, campaign_name, channel, network, offer_id))
+        conn.commit()
+
+        response_code = 200
+        message = "success"
+
+    except Exception as e:
+        response_code = 500
+        message = str(e)
+
+    return HttpResponse(json.dumps({"response_code": response_code, "message": message}))
