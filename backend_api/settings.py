102d5bc27353deeede66151f34664b3f73458488
deepeshmotwani <deepesh@appsuccessor.com>
2023-12-19 18:11:37 +0530

    added env and db

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
-
+import os
+import dotenv
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,10 +76,17 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': os.getenv('DB_NAME'),  
+        'USER': os.getenv('DB_USER'),  
+        'PASSWORD': os.getenv('DB_PASS'),  
+        'HOST': os.getenv('DB_HOST'),  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
---
+++ team2b/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Team2BConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'team2b'
