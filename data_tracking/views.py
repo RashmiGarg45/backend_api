5b2d779c2aac3bae77a34f72253423b9ce84bc06
rash_bitbucket <rashmi@appanalytics.in>
2024-01-06 17:59:59 +0530

    appsflyerKey updated

--- data_tracking/views.py
+++ data_tracking/views.py
@@ -18,7 +18,7 @@ def tracking(request, package_name, path):
     if request.body:
         d = request.body
 
-        key = "R2PcxTzKKaWVyMakREG6X9".encode("utf-8")
+        key = "VToVBTxx6TJUB4H6fNkdWE".encode("utf-8")
         key = pbkdf2_hmac(hash_name='sha1', password=bytes(key), salt=bytearray([0] * 8), iterations=10000, dklen=16)
         cipher = AES.new(key, AES.MODE_CBC, iv=d[-24:-8])
         data = pad(d, 16)
