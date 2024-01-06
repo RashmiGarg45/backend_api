81da33189071be94ff4ba0b35a01fb9d56de36a3
rash_bitbucket <rashmi@appanalytics.in>
2024-01-06 17:38:09 +0530

    data tracking api added

--- data_tracking/views.py
+++ data_tracking/views.py
@@ -1,34 +1,29 @@
 import json
 import array
+from Crypto.Util.Padding import pad
 from Crypto.Cipher import AES
+from hashlib import pbkdf2_hmac
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
-def pad(s):
-	bs = AES.block_size
-	return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+# def pad(s):
+#     bs = AES.block_size
+#     s = str(s)
+#     print ((bs - len(s) % bs) * chr(bs - len(s) % bs))
+#     return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
 
 @api_view(['POST', 'GET'])
 def tracking(request, package_name, path):
+    import base64
     if request.body:
-        d = json.loads(request.body)
-        data = []
-        for i in bytearray(d):
+        d = request.body
 
-            if i > 127:
-                data.append(-256+i)
-            else:
-                data.append(i)	
+        key = "R2PcxTzKKaWVyMakREG6X9".encode("utf-8")
+        key = pbkdf2_hmac(hash_name='sha1', password=bytes(key), salt=bytearray([0] * 8), iterations=10000, dklen=16)
+        cipher = AES.new(key, AES.MODE_CBC, iv=d[-24:-8])
+        data = pad(d, 16)
+        new_data = cipher.decrypt(data)#.replace('\n', '')
 
-        key = [100, 45, -19, -50, 81, 127, -26, -20, -98, -35, -61, -99, -10, 78, 70, -83]
-        val = data[-24::]
-        iv = val[:16]
-        key_new = array.array('b', key).tostring()
-        iv_new = array.array('b', iv).tostring()
-        cipher = AES.new(key_new, AES.MODE_CBC, iv_new )
-        data = array.array('b', data).tostring()
-        data = pad(data)	
-        decrypted_text = cipher.decrypt( data )
-        print (decrypted_text)
+        print (new_data)
 
     return HttpResponse(json.dumps({"package_name": package_name, "path": path}))
