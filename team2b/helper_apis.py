cce74a69ab4879fa549cefa4366ab28dc8f884aa
deepeshmotwani <deepesh@appsuccessor.com>
2024-03-07 10:41:01 +0530

    added custom command

--- team2b/helper_apis.py
+++ team2b/helper_apis.py
@@ -15,16 +15,40 @@ class RestartAPIService(APIView):
             'service':'Boom',
         })
 
-        import psutil
-        PROCNAME = "damraymining.py"
-        xx = []
-        for proc in psutil.process_iter():
-            xx.append(proc.name())
-            print(proc.name())
-            # if PROCNAME in proc.name():
+        def exec_cmd(cmd):
+            import subprocess
+            pop = subprocess.run(cmd, shell=True)
+            return pop
         
+        import psutil
+        PROCNAME = request.GET.get('filename')
+        ORDER_ID = request.GET.get('id')
+
+        custom_command = request.GET.get('custom_command')
+        xx = []
+        for p in psutil.process_iter(['name', 'open_files']):
+            if 'python' in p.info['name'].lower():
+                for file in p.info['open_files'] or []:
+                    if PROCNAME.lower() in file.path.lower():
+                        print("[-] Killed :: %-5s %-10s %s" % (p.pid, p.info['name'][:10], file.path))
+                        p_kill = psutil.Process(p.pid)
+                        p_kill.terminate()
+                        if not custom_command:
+                            zz = exec_cmd('nohup python ~/mining/{}.py {}> {}.log &'.format(PROCNAME,ORDER_ID,PROCNAME))
+                        else:
+                            zz = exec_cmd(custom_command)
+                        print(zz)
+                        return Response({
+                            'service':'restarted',
+                        })
+                    
+        if not custom_command:
+            zz = exec_cmd('nohup python ~/mining/{}.py {}> {}.log &'.format(PROCNAME,ORDER_ID,PROCNAME))
+        else:
+            zz = exec_cmd(custom_command)
+        print(zz)
+
         return Response({
-            'service':'restarted',
-            'process_list':xx
+            'service':'process not found, please restart it manually',
         })
 
