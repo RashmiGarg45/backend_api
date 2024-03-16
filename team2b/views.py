52cbc38e06b00123bcbcf93e3bc5e6e5ac26bd56
deepeshmotwani <deepesh@appsuccessor.com>
2024-03-16 14:08:07 +0530

    added habib changes

--- team2b/views.py
+++ team2b/views.py
@@ -365,6 +365,9 @@ class HabibOrderIdConstants(APIView):
             setUsed = False
         
         filter_dict = {}
+        if request.GET.get('old_ids'):
+            filter_dict['created_at__lte'] = '2024-02-10'
+            
         if order_status:
             filter_dict['order_status'] = order_status
         query = HabibScriptOrderIdsConstants.objects.filter(used_at=None,**filter_dict).order_by('-created_at')[0:50].first()
