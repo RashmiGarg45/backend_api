72c1f89b3d8aeade5a0c553389b5d8323c327abc
deepeshmotwani <deepesh@appsuccessor.com>
2024-07-30 10:25:01 +0530

    added redis for order ids

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -189,11 +189,12 @@ DATABASES = {
 }
 
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://localhost:6379/",
-        "KEY_PREFIX": "imdb",
-        "TIMEOUT": 60 * 15,  # in seconds: 60 * 15 (15 minutes)
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
     }
 }
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
