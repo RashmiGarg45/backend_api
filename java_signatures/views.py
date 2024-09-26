ef9e94c2cd354c19e4e943b7c9144ac4c2aec841
rash_bitbucket <rashmi@appanalytics.in>
2024-09-26 13:09:37 +0530

    error resolved

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -1232,7 +1232,7 @@ def put_data(request):
 
             for user_id in data:
                 if str(user_id) not in already_present_user_ids:
-                    created_at = datetime.fromtimestamp(time.time()).strftime("%d-%m-%Y %H:%M:%S:%f")[:-3]
+                    created_at = datetime.datetime.fromtimestamp(time.time()).strftime("%d-%m-%Y %H:%M:%S:%f")[:-3]
                     cursor.execute('''INSERT INTO pocket52_userId (user_id, created_at, isUsed)
                                         VALUES ('{}','{}', 0)'''.format(user_id ,created_at ))
                     conn.commit()
