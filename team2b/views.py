6bfb678870f4a26366ac02eb5fa376fb20cae7b0
rash_bitbucket <rashmi@appanalytics.in>
2025-03-11 16:23:05 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -1923,6 +1923,7 @@ class RevenueHelperAPI(APIView):
         query.app_version = request.data.get('app_version', '')
         query.script_version = request.data.get('script_version', '')
         query.day = request.data.get("day")
+        query.c_day = request.data.get("day")
         try:
             query.save()
             return Response({
