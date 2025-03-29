25215f3e441475304cab7a0a96f1bff7982045f3
rash_bitbucket <rashmi@appanalytics.in>
2025-03-30 02:02:50 +0530

    updated

--- java_signatures/urls.py
+++ java_signatures/urls.py
@@ -66,5 +66,5 @@ urlpatterns = [
     path("get_bottles_orders_count/", views.get_bottles_orders_count, name="get_bottles_orders_count"),
     path("get_tajrummy_userId/", views.get_tajrummy_userId, name="get_tajrummy_userId"),
     path("get_tajrummy_users_count/", views.get_tajrummy_users_count, name="get_tajrummy_users_count"),
-    path("get_event_info/", views.get_data, name="get_data"),
+    path("get_event_data/", views.get_data, name="get_data"),
 ]
