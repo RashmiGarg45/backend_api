d949b0f11be3c2c4498fef3ef7e8cfa983a167ee
rash_bitbucket <rashmi@appanalytics.in>
2025-04-04 16:21:41 +0530

    updated

--- java_signatures/urls.py
+++ java_signatures/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from java_signatures import views
 
 urlpatterns = [
-    path("get_signature/", views.get_signtaure, name="get_signtaure"),
+    path("new_get_signature/", views.get_signtaure, name="get_signtaure"),
     path("get_tatapalette_orders/", views.get_tatapalette_orders, name="get_tatapalette_orders"),
     path("get_available_orders_count/", views.get_available_orders_count, name="get_available_orders_count"),
     path("add_install_count/", views.add_install_count, name="add_install_count"),
@@ -13,7 +13,7 @@ urlpatterns = [
     path("get_zalora_orders/", views.get_zalora_orders, name="get_zalora_orders"),
     path("get_zalora_orders_count/", views.get_zalora_orders_count, name="get_zalora_orders_count"),
     path("update_zalora_orderid_status/", views.update_zalora_orderid_status, name="update_zalora_orderid_status"),
-    path("ragazzo_signature/", views.ragazzo_signature, name="ragazzo_signature"),
+    path("new_ragazzo_signature/", views.ragazzo_signature, name="ragazzo_signature"),
     path("get_samco_user_data/", views.get_samco_user_data, name="get_samco_user_data"),
     path("get_samco_users_count/", views.get_samco_users_count, name="get_samco_users_count"),
     path("get_flappdeals_orders_count/", views.get_flappdeals_orders_count, name="get_flappdeals_orders_count"),
