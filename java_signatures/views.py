e235ffdd4970db92a72ba9fcd0f38e98445cbb22
rash_bitbucket <rashmi@appanalytics.in>
2025-04-07 13:39:11 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -1933,15 +1933,13 @@ class TrackEvents(APIView):
         event_day = request.GET.get("event_day")
         event_value = request.GET.get("event_value")
         revenue = float(request.GET.get("revenue", 0))
-        # offer_details = channel + "::" + network + "::" + offer_id
         
         offer_serial = InstallData(offer_serial)
-        print (offer_serial)
 
         event_data = EventInfo.objects.filter(campaign_name=campaign_name, offer_serial=offer_serial, event_name=event_name, event_day=event_day)
 
         if not event_data:
-            event_details = EventInfo(campaign_name=campaign_name, offer_serial=offer_serial, event_name=event_name, event_count=1, event_day=event_day, revenue=revenue)
+            event_details = EventInfo(campaign_name=campaign_name, offer_serial=offer_serial, event_name=event_name, event_count=1, event_day__lte=event_day, revenue=revenue)
         else:
             event_details = event_data.get()
             event_details.event_count += 1
