f1096021255f4208c820fff0157900cc3c12b18e
rash_bitbucket <rashmi@appanalytics.in>
2025-05-19 19:02:23 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2118,16 +2118,16 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id):
         return {0:85, 1:80, 2:60, 3:45, 4: 33}
 
     elif campaign_name == "bingofrenzytmodd" and event_name == "1_99":
-        return {0:41, 1:41}
+        return {0:7, 1:7}
     
-    elif campaign_name == "bingofrenzytmodd" and event_name == "19_99":
-        return {1:68, 2:68, 3:41, 4:41}
+    # elif campaign_name == "bingofrenzytmodd" and event_name == "19_99":
+    #     return {1:68, 2:68, 3:41, 4:41}
     
     elif campaign_name == "bingofrenzytmodd" and event_name == "4_99":
-        return {0:44, 1:44, 2:33, 3:33}
+        return {4:7, 5:7}
     
     elif campaign_name == "bingofrenzytmodd" and event_name == "9_99":
-        return {0:33, 3:25, 4:20, 5:16.6, 6:14.2, 7:12.5}
+        return {2:7, 3:7}
 
 
 class checkEligibility(APIView):
