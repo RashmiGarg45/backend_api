10054d0fa252bff934373496cf2fd0115d0c77c3
rash_bitbucket <rashmi@appanalytics.in>
2025-06-24 13:55:01 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -100,7 +100,7 @@ class GenericScriptFunctions(APIView):
             ids_mined[key] = tablesDict[key].objects.filter(created_at__gte=str(today),created_at__lte=str(today+" 23:59:59")).count()
 
             if key == "indigomoddteam2modd_OID":           
-                ids_mined[key] = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now()).exclude(company__in=private_companies).count()
+                ids_mined[key] = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),created_at__gte=str(today),created_at__lte=str(today+" 23:59:59")).exclude(company__in=private_companies).count()
 
         from data_tracking.util import googleChatBot_send_message
         message = {
