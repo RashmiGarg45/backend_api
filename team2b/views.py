6245e0c16bc99a08f0aa3e66809d82ec12b22853
rash_bitbucket <rashmi@appanalytics.in>
2025-06-26 10:20:41 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -1949,7 +1949,7 @@ class IndigoV2MiningAPI(APIView):
             bt3_count = IndigoV2Mining.objects.filter(used_at__startswith=datetime.now().strftime('%Y-%m-%d'), channel__in=("mobpine", "77ads", "appamplify")).count()
             print (bt3_count)
 
-            if bt3_count > 120:
+            if bt3_count > 150:
                 return Response({'body':{"status": "Not Allowed"}})
 
         # elif channel in ["quasarmobi", "offersinfinite", "mobiaviator"]:
@@ -1961,7 +1961,7 @@ class IndigoV2MiningAPI(APIView):
         elif channel in ["adshustle", "vestaapps", "appsfollowing"]:
             bt2_count = IndigoV2Mining.objects.filter(used_at__startswith=datetime.now().strftime('%Y-%m-%d'), channel__in=("adshustle", "vestaapps", "appsfollowing")).count()
             print (bt2_count)
-            if bt2_count > 120:
+            if bt2_count > 150:
                 return Response({'body':{"status": "Not Allowed"}})
 
 
