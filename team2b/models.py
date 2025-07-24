4982ef4dc804fcde8877f92a3442f19f746a2411
Harsh kaushik <harsh@appanalytics.in>
2025-07-24 11:05:27 +0530

    updated

--- team2b/models.py
+++ team2b/models.py
@@ -2299,3 +2299,17 @@ class OkeyvipMining(models.Model):
         indexes = [
             models.Index(fields=['id']),
         ]
+
+class Moneymetmodduid(models.Model):
+
+    serial = models.AutoField(primary_key=True, editable=False)
+    campaign_name = models.CharField(max_length=20,default='moneymetmodd')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.CharField(max_length=50,unique=True)
+    used_at = models.DateTimeField(default = None,blank=True, null=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['id']),
+        ]
