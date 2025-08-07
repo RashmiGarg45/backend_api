72c24ea5874c6c92481d9963fcb58bf173a58dbb
rash_bitbucket <rashmi@appanalytics.in>
2025-08-07 19:15:59 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -2013,12 +2013,12 @@ class IndigoV2MiningAPI(APIView):
         
         cc = 21
         if not unused_count:
-            cc = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='None',**filter_dict).order_by('departure_date', 'created_at').count()
+            cc = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='None',**filter_dict).order_by('created_at', 'departure_date').count()
         
         if unused_count or (not unused_count and cc>20):
-            query = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='None',**filter_dict).order_by('departure_date', 'created_at')[0:50].first()
+            query = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='None',**filter_dict).order_by('created_at', 'departure_date')[0:50].first()
         if not query:
-            query = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='C*****y',**filter_dict).order_by('departure_date', 'created_at')[0:50].first()
+            query = IndigoV2Mining.objects.filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),company='C*****y',**filter_dict).order_by('created_at', 'departure_date')[0:50].first()
         if not query:
             # query = IndigoV2Mining.objects.annotate(fare_float=Cast('fare', FloatField())).filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),**filter_dict, fare_float__gt=1000.0).exclude(company__in=private_companies).order_by('departure_date', 'created_at')[0:50].first()
             query = IndigoV2Mining.objects.annotate(fare_float=Cast('fare', FloatField())).filter(used_at=None, currency="INR",departure_date__gte=datetime.now(),**filter_dict, fare_float__gt=1000.0).exclude(company__in=private_companies).order_by('created_at', 'departure_date')[0:50].first()
