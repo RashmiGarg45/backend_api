691106699296b968ce8613ba4640efb53ec7fb56
Harsh kaushik <harsh@appanalytics.in>
2025-08-18 12:02:15 +0530

    update

--- team2b/urls.py
+++ team2b/urls.py
@@ -162,4 +162,5 @@ urlpatterns = [
     path("myauchan", views.MyauchanAPI.as_view(), name="cru_myauchan"),
     path("ikea", views.IkeaAPI.as_view(), name="cru_ikea"),
     path("shopeebrUID", views.ShopeebrUIDAPI.as_view(), name="cru_shopeebrUID"),
+    path("shopeeth", views.ShopeethUIDAPI.as_view(), name="cru_shopeethUID"),
 ]
