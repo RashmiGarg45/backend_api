02d99a6df1e57e0539d6579390a369ad624adcc5
rash_bitbucket <rashmi@appanalytics.in>
2025-08-28 17:06:48 +0530

    updated

--- data_tracking/urls.py
+++ data_tracking/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("report6/statistics", report_view.Report6Stats.as_view(), name="r6_stats"),    
     path("sheet/report6", report_view.Report6UpdateOnSheet.as_view(), name="r6_stats"),    
     path("chatbot/notify/bt_not_running_past_2_months", report_view.ChatBotNotRunLastTwoMonthLevel2.as_view(), name="bot"),  
-    path("sheetUpdate/watcho", report_view.WatchoUpdateSheet.as_view(), name="bot"),    
-    path("<str:package_name>/<path:path>", views.tracking, name="tracking"),    
+    path("sheetUpdate/watcho", report_view.WatchoUpdateSheet.as_view(), name="bot"),         
     path("sheet/validation", report_view.UpdateValidationSheet.as_view(), name="update"), 
+    path("<str:package_name>/<path:path>", views.tracking, name="tracking"),  
 ]
