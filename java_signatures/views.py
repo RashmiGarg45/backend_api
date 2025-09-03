79ab71b2fc031c83b1c4ad8d83acd7ab51c46709
Harsh kaushik <harsh@appanalytics.in>
2025-09-03 12:50:39 +0530

    update

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2427,6 +2427,9 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id):
     elif campaign_name == "coinmenaauto" and event_name == "aj5y08":
         return {0:90, 1:25, 2:16, 3:14}
 
+    elif campaign_name == "smilesauto" and event_name == "a6ou21":
+        return {0:45, 1:20, 2:14, 3:11}
+
 class checkEligibility(APIView):
     def get(self, request):
         campaign_name = request.GET.get('campaign_name')
