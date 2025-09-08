ba30396c46080df9022019ef42da6c32fbcb549b
Harsh kaushik <harsh@appanalytics.in>
2025-09-08 16:15:18 +0530

    update

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -1913,7 +1913,17 @@ class TrackInstalls(APIView):
         network = request.GET.get("network")
         offer_id = request.GET.get("offer_id")
         currency = request.GET.get("currency", "USD")
-        date = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
+        updated_timezone = request.GET.get("updated_timezone", None)
+        if updated_timezone:
+            if '-' in updated_timezone:
+                updated_timezone=updated_timezone.split('-')[1]
+                date = (datetime.datetime.utcnow() - datetime.timedelta(hours=updated_timezone)).strftime("%Y-%m-%d")
+            else:
+                if '+' in updated_timezone:
+                    updated_timezone=updated_timezone.split('+')[1]
+                date = (datetime.datetime.utcnow() - datetime.timedelta(hours=updated_timezone)).strftime("%Y-%m-%d")
+        else:
+            date = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
 
         if not all([campaign_name, channel, network, offer_id]):
             return Response({"status": 400,"message": "Missing required parameters","data": {}})
