f2da31ccf30b9a313f4a9537e57fd4b924b145cb
rash_bitbucket <rashmi@appanalytics.in>
2025-09-10 18:38:42 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2485,6 +2485,12 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id):
     
     elif campaign_name == "shorttvtmodd" and event_name == "revenue_999":
         return {3:90}
+    
+    elif campaign_name == "etomomodd" and event_name == "loan_requested":
+        return {0:7}
+    
+    elif campaign_name == "cryptocomtmodd" and event_name == "kyc_approved_push_sent":
+        return {0:7}
 
 class checkEligibility(APIView):
     def get(self, request):
