cb644d3b094648cc7c2d3ff5d2f9d67f5a6e1540
rash_bitbucket <rashmi@appanalytics.in>
2025-09-11 21:14:12 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -1934,7 +1934,7 @@ class TrackInstalls(APIView):
             return Response({"status": 400,"message": "Missing required parameters","data": {}})
         
         if required_timezone:
-            print ("kfc", date)
+            print ("kfc", date, type(date))
             install_data = InstallDataTZ.objects.filter(campaign_name=campaign_name, created_at=date, channel=channel, network=network, offer_id=offer_id)
         else:
             install_data = InstallData.objects.filter(campaign_name=campaign_name, created_at=date, channel=channel, network=network, offer_id=offer_id)
