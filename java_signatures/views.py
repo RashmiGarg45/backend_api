4a93db3339b0de1f3ab7ac7784b8ca63fe30ee78
rash_bitbucket <rashmi@appanalytics.in>
2025-09-12 11:49:02 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2001,7 +2001,7 @@ def events_per_day_stats(campaign_name, event_name, channel, network, offer_id):
         return 12
     
     elif campaign_name == "kfcmexicotmodd":
-        return 15 #percentage
+        return 18 #percentage
 
 def camp_wise_stats(campaign_name, event_name, channel, network, offer_id):
     if campaign_name == "quickcashonlinemodd" and event_name == "approvals_cnt_server":
