621878a25a0a1908026e083135ddb7e6c6845e1b
Harsh kaushik <harsh@appanalytics.in>
2025-10-10 14:59:56 +0530

    update

--- team2b/urls.py
+++ team2b/urls.py
@@ -187,4 +187,5 @@ urlpatterns = [
     path("clubeextra", views.ClubeextraAPI.as_view(), name="cru_clubeextra"),
     path("clubeextracid", views.ClubeextracidAPI.as_view(), name="cru_clubeextracid"),
     path("hering", views.HeringAPI.as_view(), name="cru_hering"),
+    path("babytracker", views.Babytrackeruid.as_view(), name="cru_babytracker"),
 ]
