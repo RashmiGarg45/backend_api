7f03981f3f06f1b8995fd0f32fa742db60a554c2
Harsh kaushik <harsh@appanalytics.in>
2025-10-10 10:35:13 +0530

    update

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2472,6 +2472,11 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id):
     elif campaign_name == "heliummobilemodd" and event_name == "app_purchase_free" and offer_id:
         if offer_id.isdecimal():
             return {0:5, 1:3} # {0:5, 1:4.5}
+        else:
+            return {0:30, 1:20, 2:16}
+
+    elif campaign_name == "heliummobilemodd" and event_name == "purchase":
+        return {0:45, 1:30, 2:25}
 
     elif campaign_name == "abhibusauto" and event_name == "cukuuk" and offer_id:
         if offer_id.isdecimal():
