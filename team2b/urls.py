bff4c5d92a9f5b7ddbf196ebef715833d1249548
Harsh kaushik <harsh@appanalytics.in>
2025-12-02 19:01:48 +0530

    update

--- team2b/urls.py
+++ team2b/urls.py
@@ -205,4 +205,5 @@ urlpatterns = [
     path("wizely", views.WizelyMiningAPI.as_view(), name="cru_wizely_mining"),
     path("timoclubuid", views.TimoclubuidAPI.as_view(), name="cru_timoclubuid"),
     path("mambanew", views.MambanewUIDAPI.as_view(), name="cru_mambanew"),
+    path("wizelyuid", views.WizelyUIDAPI.as_view(), name="cru_wizelyuid"),
 ]
