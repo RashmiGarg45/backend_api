b81fb51461f2d3d13f0d9f3c2805d6c5506abba5
rash_bitbucket <rashmi@appanalytics.in>
2025-12-08 10:48:05 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -7047,7 +7047,7 @@ class GalaxyChatCountryAPI(APIView):
         query_list = GalaxyChatCountry.objects.filter(used_at=None,from_selfcall__contains = "alse").exclude(**exclude_dict_1).order_by('-created_at')[0:25].all()        
         if not query_list:
             print ("Galaxy in not function")
-            query_list = GalaxyChatCountry.objects.exclude(**exclude_dict).order_by('-created_at')[0:25].all()
+            query_list = GalaxyChatCountry.objects.filter(from_selfcall__contains = "alse").exclude(**exclude_dict).order_by('-created_at')[0:25].all()
 
         if query_list:
             print ("Galzxy found query")
