23e05d2c2f96dcc423522d8146c2315039378f34
Harsh kaushik <harsh@appanalytics.in>
2025-12-10 18:10:37 +0530

    update

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2340,6 +2340,16 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
 
         return {0: 45, 1:23}
 
+    elif campaign_name == "bcsinvestiosmodd" and event_name == "93441_Onboard_OpenAcc_SignDocsSmsTap":
+        return {0: 6.2, 1:5, 2: 4.34, 3:4, 4:3.84}
+
+    elif campaign_name == "bcsinvestiosmodd" and event_name == "28500_Instr_Bid_Success":
+
+        if channel=="adshustle" and offer_id in ["bcsmnditvammp", "bcsmicovammp"]:
+            return {0:9}
+
+        return {0: 45, 1:23}
+
     elif campaign_name == "indigomoddteam2modd" and event_name == "af_purchase" and channel in ["mobpine", "77ads", "appamplify"]:
         return {0:12, 1:10, 2:9}
     
