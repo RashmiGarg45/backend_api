d59baeeaffd6b53a98c840902ed4e38a2f1d9f69
rash_bitbucket <rashmi@appanalytics.in>
2026-01-12 13:02:44 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -3033,7 +3033,7 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
         return {0:14, 1:9.5, 2:8}
 
     elif campaign_name == "rootcarinsurancemodd" and event_name == "Profile":
-        return {0: 9, 1:7, 2: 6}
+        return {0: 9, 1:6, 2: 5} #{0: 9, 1:7, 2: 6}
 
     elif campaign_name == "mcluckcasinoiosmodd" and event_name == 'first_purchase':
         return {0:90, 1:45}
