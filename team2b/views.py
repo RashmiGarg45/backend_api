98892621fe6a40604326d6774ebcbfb9200607bc
rash_bitbucket <rashmi@appanalytics.in>
2026-01-16 11:11:29 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -9975,4 +9975,6 @@ class RevenueHelperStats(APIView):
                 created_at__gt=date_,
             )
         )
-        return Response({"response_code": 200, "message": "success", "data": json.dumps({"install_count": install_count})})
+
+        print (install_count)
+        return Response({"response_code": 200, "message": "success", "data": {"install_count": install_count[0]}})
