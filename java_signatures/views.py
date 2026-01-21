788ae4792bc9edfbe8e682e04facbf26c09d4d98
Harsh kaushik <harsh@appanalytics.in>
2026-01-21 16:44:14 +0530

    update

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2225,10 +2225,12 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
         return {0:16.6, 1:11.11, 2:8.33, 3:7.14, 4:6.6}
 
     elif campaign_name == "onmobileauto" and event_name == "jr95xb":
-        return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
+        # return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
+        return {0:1.81, 1:1.33, 2:1.11}
 
     elif campaign_name == "onmobilautoios" and event_name == "jr95xb":
-        return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
+        # return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
+        return {0:1.81, 1:1.33, 2:1.11}
     
     elif campaign_name == "magztermodd" and event_name == "mg_1month_freetrial":
         return {0:20, 1:14.2, 2:11.11, 3:10}
