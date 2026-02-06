7f5f87ed5a59e3ec73f0e11de4825efbb48ecf5b
rash_bitbucket <rashmi@appanalytics.in>
2026-02-06 19:03:19 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2826,6 +2826,13 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
     elif campaign_name == "williamhillsportiosmodd" and event_name == 'FTD':
         if offer_id in ["apps-whsports.ios", "whuk2adgmmp"]:
             return {0:45}
+
+        elif offer_id in ["willadghubmmp"]:
+            return {0:50}
+        
+        elif offer_id in ["whsptadgmmp"]:
+            return {0:60}
+
         return {0:30, 1:22.5, 2:18}
 
     # elif campaign_name == "vegasiosmodd" and event_name == 'FTD':
