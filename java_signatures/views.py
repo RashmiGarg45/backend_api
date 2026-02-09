7da6cbc47e4f9f6b506fa78303c1dd8e921080bc
rash_bitbucket <rashmi@appanalytics.in>
2026-02-09 16:41:27 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2230,11 +2230,11 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
 
     elif campaign_name == "onmobileauto" and event_name == "jr95xb":
         # return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
-        return {0:1.81, 1:1.33}#, 2:1.29}
+        return {0:1.81, 1:1.25}#, 2:1.29}
 
     elif campaign_name == "onmobilautoios" and event_name == "jr95xb":
         # return {0:1.81, 1:1.56, 2:1.42, 3:1.33}
-        return {0:1.81, 1:1.33}#, 2:1.29}
+        return {0:1.81, 1:1.25}#, 2:1.29}
     
     elif campaign_name == "magztermodd" and event_name == "mg_1month_freetrial":
         return {0:20, 1:14.2, 2:11.11, 3:10}
