66c1f40bc23d7404d555c22d3a563194f43855d0
rash_bitbucket <rashmi@appanalytics.in>
2026-02-10 15:07:06 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -3134,6 +3134,9 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
         return {0:90, 1:45, 2:30}
 
     elif campaign_name == "dominosturkeyauto" and event_name == "yfub50":
+
+        if offer_id in ["domdoummp", "dom2doummp"]:
+            return {0:14, 1:3.52, 2:2.7}
         
         return {0:14, 1:3.52} # need to update
 
