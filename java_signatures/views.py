58cce12175e18b1e16719aa40fcc069cbbb180b3
rash_bitbucket <rashmi@appanalytics.in>
2026-03-07 10:49:37 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -1803,7 +1803,7 @@ def camp_wise_stats(campaign_name, event_name, channel, network, offer_id,Pay_ou
     elif campaign_name == "shionauto" and event_name == "10000_revenue":
         from datetime import datetime
         today = datetime.now().weekday()
-        if today in [0,3]:
+        if today in [0,2]:
             return {0:37}    
     
     elif campaign_name == "paymayamodd" and event_name == "EKYC_SUCCESS":
