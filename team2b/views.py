39e06cccb49c1b046571b03cd824eade263040f6
rash_bitbucket <rashmi@appanalytics.in>
2026-04-02 17:50:49 +0530

    updated

--- team2b/views.py
+++ team2b/views.py
@@ -384,18 +384,18 @@ class GenericUnusedIdScriptFunctions(APIView):
         ids_mined = {}
         for key in tablesDict.keys():
             query = tablesDict[key].objects.filter(used_at = None).order_by('created_at')
-            data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d")}
+            data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d") if query else "NA"}
             ids_mined[key] = data
 
             if key == "indigomoddteam2modd_OID":
                 query  = IndigoV4Mining.objects.filter(used_at=None,departure_date__gte=datetime.now()).order_by('created_at')
 
-                data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d")}
+                data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d") if query else "NA"}
                 ids_mined[key] = data
 
             if key == "galaxy_ru_uid":
                 query = GalaxyChatCountry.objects.filter(used_at = None).filter(Q(from_selfcall__contains="alse") |Q(from_selfcall__contains="Report")).order_by('created_at')
-                data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d")}
+                data = {"total_count": query.count(), "oldest_id": query.first().created_at.strftime("%Y-%m-%d") if query else "NA"}
                 ids_mined[key] = data
 
             # if key == "lottermxiosmodd_UID":
