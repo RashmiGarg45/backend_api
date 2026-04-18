aa1b213d785d6563107b2effdd55abf7da07c86e
rash_bitbucket <rashmi@appanalytics.in>
2026-04-18 12:34:53 +0530

    updted

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2681,7 +2681,7 @@ class Running_camps_stats(APIView):
 
         print (filter_dict)
 
-        installs_qs = InstallData.objects.filter(created_at__range=(from_date, to_date),campaign_name=campaign_name)
+        installs_qs = InstallData.objects.filter(created_at__range=(from_date, to_date),campaign_name=campaign_name, **filter_dict)
 
         print ("&"*100)
         print (installs_qs)
