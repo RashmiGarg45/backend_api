43688ae46e25f1e904b5b5ad5a3325b7cc4e1716
ashfaq311 <ashfaq@appanalytics.in>
2026-04-21 15:49:42 +0530

    credito365iosmodd randomness added

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2006,6 +2006,11 @@ def get_stats(campaign_name):
         "aaavpnt1iosmodd": {
             "apphud_trial_started": {0:22.5, 1:15, 2:11.5, 3:9.9}
         },
+
+        "credito365iosmodd": {
+            'first_loan': {0: 40, 1: 22.23, 2: 18.18, 3: 16.67}
+        }
+
         
 
     }
