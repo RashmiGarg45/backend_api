2060ebc531a090a0f721e220b219824b0a5067c6
rash_bitbucket <rashmi@appanalytics.in>
2026-04-22 12:24:22 +0530

    updated

--- java_signatures/views.py
+++ java_signatures/views.py
@@ -2009,7 +2009,7 @@ def get_stats(campaign_name):
 
         "credito365t1iosmodd": {
             'first_loan': {0: 40, 1: 22.23, 2: 18.18, 3: 16},
-             'repeat_loan': {2: 125, 3: 100}
+             'repeat_loan': {3: 150}
         },
 
         "globoplaytmodd": {
